c1c49e36aa1e20310e283401fd8d241c81bd318e
Emre Kocak <m.emrekocakk@gmail.com>
2021-08-24 22:14:24 +0300

    Initial Commit

---
+++ TimerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerProject
+//
+//  Created by Emre Koçak on 24.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
